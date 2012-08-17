fac0127076cfa5c687924d73c196f54b12ce26df
Steve Lair <steve.lair@gmx.de>
2012-08-17 16:03:20 +0200

    Stand direkt nach Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jukebox::Application.config.secret_token = 'a285eefbbab9c541fc43da50be22e57340f45290483832bf65c2bcbb9b67feb1f73703a354a5aaea0f1990d7e8d3972841ae4da462c6b4cc387d2706220c0c7c'
